770c7b40e877670046ad3a57905c863551c445b9
alejo-martinez <alejoomartinex11@gmail.com>
2025-03-03 19:42:51 -0300

    domain eliminated

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -5,7 +5,7 @@ const login = async (req, res, next) => {
     try {
         const user = req.user;
         const accesToken = generateToken(user);
-        return res.cookie('accesToken', accesToken, { maxAge: 6 * 60 * 60 * 8000, signed: true, httpOnly: true, secure: true, sameSite: 'None', domain:'.vercel.app' }).send({ status: 'success', message: 'Logueado !', payload: user });
+        return res.cookie('accesToken', accesToken, { maxAge: 6 * 60 * 60 * 8000, signed: true, httpOnly: true, secure: true, sameSite: 'None' }).send({ status: 'success', message: 'Logueado !', payload: user });
     } catch (error) {
         next(error);
     }
@@ -35,7 +35,7 @@ const logOut = async (req, res, next) => {
                 // res.send({ status: 'error', message: 'No pudimos cerrar la sesion: ' + error });
                 return next(error);
             }
-                res.clearCookie('accesToken', { domain: '.vercel.app'}).send({ status: 'success', message: 'Sesión cerrada con éxito !' })
+                res.clearCookie('accesToken').send({ status: 'success', message: 'Sesión cerrada con éxito !' })
         })
     } catch (error) {
         next(error);
