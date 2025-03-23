ce6c16ad911bb32c29e6a67636e1a36656b1a15b
alejo-martinez <alejoomartinex11@gmail.com>
2025-03-23 14:18:35 -0300

    fix products add to cart

--- src/app.js
+++ src/app.js
@@ -95,6 +95,7 @@ io.on('connection', async (socket) => {
     socket.on('searchCode', async(data)=>{
         try {
             const prod = await ProductManager.getBy('code', Number(data.query));
+            console.log(prod)
             io.emit('resultCode', {producto: prod});
         } catch (error) {
             io.emit('errorCode', {error: error.message})
