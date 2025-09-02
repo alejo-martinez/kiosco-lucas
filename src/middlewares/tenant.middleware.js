706962dd5b832577296c0019654ec0f767eb7ecf
alejo-martinez <alejoomartinex11@gmail.com>
2025-09-02 15:43:20 -0300

    fix cors socket

--- src/middlewares/tenant.middleware.js
+++ src/middlewares/tenant.middleware.js
@@ -2,17 +2,20 @@ import { getTenantConnection } from "../tenants/connManager.js";
 import { getTenantConfig } from "../tenants/registry.js";
 
 const extractSlugFromHost = (host) => {
-    // soporta: kiosco1-tuapp.vercel.app  -> "kiosco1"
-    //          kiosco1.midominio.com     -> "kiosco1"
     if (!host) return null;
-    const h = host.toLowerCase();
 
-    // Caso vercel: kiosco1-tuapp.vercel.app
-    if (h.includes("-") && h.endsWith(".vercel.app")) {
-        return h.split(".")[0].split("-")[0];
+    // eliminar protocolo si lo tiene
+    let h = host.toLowerCase().replace(/^https?:\/\//, "");
+
+    // quitar ruta si existe
+    h = h.split("/")[0];
+
+    // caso vercel: kiosco-test.vercel.app -> kiosco-test
+    if (h.endsWith(".vercel.app")) {
+        return h.replace(".vercel.app", "");
     }
 
-    // Caso subdominio normal: kiosco1.midominio.com
+    // caso subdominio normal: kiosco-lucas-front.midominio.com -> kiosco-lucas-front
     return h.split(".")[0];
 }
 
