6f70d272a8aaf9c35dd4226525fc7cbc4f7f4737
alejo-martinez <alejoomartinex11@gmail.com>
2025-10-30 20:01:08 -0300

    fix socket

--- src/app.js
+++ src/app.js
@@ -46,7 +46,8 @@ const io = new Server(httpServer, {
             "https://kiosco-lucas-front.vercel.app",
             "https://kiosco-test.vercel.app"
         ],
-        credentials: true
+        credentials: true,
+        methods: ["GET", "POST"]
     }
 });
 
